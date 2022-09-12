221650c1d25591d48f83821ab395ae93991c55a4
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-12 19:14:45 +0200

    init push

---
+++ index.ts
@@ -0,0 +1,62 @@
+import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
+
+async function gvm(req: Request): Promise<Response> {
+  const parameters = new URL(req.url).searchParams;
+
+  for (const [key, value] of parameters) {
+    console.log(`${key}: ${value}`);
+  }
+
+  const assembly = parameters.get("assembly") || "";
+  const input = parameters.get("input") || "";
+
+  if (assembly === null) {
+    return new Response("assembly key is not defined");
+  }
+
+  let cmd = [];
+  // string interpolation
+  cmd.push(`echo "${assembly}" > /tmp/assembly.s`);
+  if (parameters.get("win") === "true") {
+    cmd = [
+      "cmd",
+      "/C",
+      `echo ${assembly}`,
+      ">",
+      "test.gc",
+      "&&",
+      "EML.exe",
+      "test.gc",
+    ];
+  } else {
+    cmd = ["echo", assembly, input];
+  }
+
+  try {
+    const p = Deno.run({ cmd, stdout: "piped" });
+
+    // await its completion
+    const [_, stdout] = await Promise.all([
+      p.status(),
+      p.output(),
+    ]);
+    const result = new TextDecoder().decode(stdout);
+    return new Response(result);
+  } catch (error) {
+    return new Response(error);
+  }
+}
+
+async function handler(req: Request): Promise<Response> {
+  console.log("Method:", req.method);
+
+  const url = new URL(req.url);
+
+  if (url.pathname === "/gvm" && req.method == "GET") {
+    return await gvm(req);
+  }
+
+  return new Response("Hello, World!");
+}
+
+serve(handler, { port: 6060 });
